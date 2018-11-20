bcb0e2e1ea0d5bc9accc968ea368fc3557f73d4c
森屋英治 <>
2018-11-20 10:58:41 +0900

    add golang web static html handler

--- go/src/helloweb/main.go
+++ go/src/helloweb/main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Hello World</h1>")
 	})
 	http.ListenAndServe(":8081", nil)
+	fmt.Println("golang web serer running ...")
 }
